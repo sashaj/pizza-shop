c7345e7d96d1b35ea76205bb07715bcfdb80c48f
Alexander Ivanov <alexanderivanovset@gmail.com>
2020-06-16 00:27:44 +0300

    disabled credentials in requests

--- server.js
+++ server.js
@@ -16,149 +16,176 @@ var serveStatic = require("serve-static");
 
 var path = require("path"); // defining the Express app
 
-
 var app = express(); // defining an array to work as the database (temporary solution)
 // const products = [{ title: "Hello, world (again)!" }];
 
-var products = [{
-  image: "arriva.jpeg",
-  name: "Arriva",
-  price: 12,
-  article: "P1",
-  available: true,
-  category: "small",
-  description: "Burger sauce, chicken, ranch sauce, chorizo, bell peppers, red onions, mozzarella, tomatoes, garlic"
-}, {
-  image: "cheese.jpg",
-  name: "Cheese",
-  price: 15,
-  article: "P2",
-  available: true,
-  category: "small",
-  description: "Large portion of mozzarella, tomato sauce"
-}, {
-  image: "ham-cheese.jpg",
-  name: "Ham and cheese",
-  price: 20,
-  article: "P3",
-  available: false,
-  category: "medium",
-  description: "Ham, large portion of mozzarella, cream sauce"
-}, {
-  image: "ham-mushrooms.jpeg",
-  name: "Ham and mushrooms",
-  price: 18,
-  article: "P4",
-  available: true,
-  category: "medium",
-  description: "Ham, champignons, large portion of mozzarella, tomato sauce"
-}, {
-  image: "hawaii.jpg",
-  name: "Hawaii",
-  price: 14,
-  article: "P5",
-  available: false,
-  category: "small",
-  description: "Chicken, tomato sauce, mozzarella, pineapple"
-}, {
-  image: "italian.jpg",
-  name: "Italian",
-  price: 25,
-  article: "P6",
-  available: true,
-  category: "big",
-  description: "Italian herbs, spicy pepperoni, tomato sauce, mushrooms, mozzarella, black olives"
-}, {
-  image: "mexican.jpg",
-  name: "Mexican",
-  price: 30,
-  article: "P7",
-  available: true,
-  category: "big",
-  description: "Chicken, tomato sauce, bell pepper, red onion, mozzarella, spicy jalapenos, tomatoes, salsa sauce"
-}, {
-  image: "pepperoni.jpeg",
-  name: "Pepperoni",
-  price: 27,
-  article: "P8",
-  available: true,
-  category: "big",
-  description: "Spicy pepperoni, larger portion of mozzarella, tomato sauce"
-}, {
-  image: "cheese-chicken.jpg",
-  name: "Cheese chicken",
-  price: 24,
-  article: "P9",
-  available: true,
-  category: "medium",
-  description: "Chicken, mozzarella, tomatoes, cheese sauce"
-}, {
-  image: "four-seasons.jpg",
-  name: "Four seasons",
-  price: 18,
-  article: "P11",
-  available: true,
-  category: "big",
-  description: "Larger portion of mozzarella, ham, pepperoni, feta cheese, tomatoes, champignons, tomato sauce"
-}, {
-  image: "meat.jpg",
-  name: "Meat",
-  price: 22,
-  article: "P12",
-  available: true,
-  category: "big",
-  description: "Chicken, ham, pepperoni, tomato sauce, spicy chorizo, mozzarella"
-}, {
-  image: "four-cheeses.jpg",
-  name: "Four cheeses",
-  price: 13,
-  article: "P13",
-  available: true,
-  category: "small",
-  description: "Larger portion of mozzarella, cheddar cheese, parmesan, blue cheese, cream sauce"
-}, {
-  image: "double-pepperoni.jpg",
-  name: "Double pepperoni",
-  price: 16,
-  article: "P14",
-  available: true,
-  category: "medium",
-  description: "More spicy pepperoni, larger portion of mozzarella, tomato sauce"
-}, {
-  image: "veggie.jpg",
-  name: "Veggie",
-  price: 21,
-  article: "P15",
-  available: false,
-  category: "medium",
-  description: "Tomatoes, feta cheese, champignons, bell peppers, red onions, mozzarella, black olives, tomato sauce"
-}, {
-  image: "margherita.jpg",
-  name: "Margherita",
-  price: 11,
-  article: "P16",
-  available: true,
-  category: "big",
-  description: "Larger portion of mozzarella, tomatoes, tomato sauce"
-}, {
-  image: "crazy-pepperoni.jpg",
-  name: "Crazy pepperoni",
-  price: 19,
-  article: "P17",
-  available: true,
-  category: "small",
-  description: "Chicken, spicy pepperoni, tomato sauce, mozzarella, sweet and sour sauce"
-}, {
-  image: "chicken-barbecue.jpg",
-  name: "Chicken barbecue",
-  price: 25,
-  article: "P18",
-  available: true,
-  category: "medium",
-  description: "Chicken, barbecue sauce, tomato sauce, red onion, mozarella, bacon"
-}]; // adding Helmet to enhance your API's security
+var products = [
+  {
+    image: "arriva.jpeg",
+    name: "Arriva",
+    price: 12,
+    article: "P1",
+    available: true,
+    category: "small",
+    description:
+      "Burger sauce, chicken, ranch sauce, chorizo, bell peppers, red onions, mozzarella, tomatoes, garlic",
+  },
+  {
+    image: "cheese.jpg",
+    name: "Cheese",
+    price: 15,
+    article: "P2",
+    available: true,
+    category: "small",
+    description: "Large portion of mozzarella, tomato sauce",
+  },
+  {
+    image: "ham-cheese.jpg",
+    name: "Ham and cheese",
+    price: 20,
+    article: "P3",
+    available: false,
+    category: "medium",
+    description: "Ham, large portion of mozzarella, cream sauce",
+  },
+  {
+    image: "ham-mushrooms.jpeg",
+    name: "Ham and mushrooms",
+    price: 18,
+    article: "P4",
+    available: true,
+    category: "medium",
+    description: "Ham, champignons, large portion of mozzarella, tomato sauce",
+  },
+  {
+    image: "hawaii.jpg",
+    name: "Hawaii",
+    price: 14,
+    article: "P5",
+    available: false,
+    category: "small",
+    description: "Chicken, tomato sauce, mozzarella, pineapple",
+  },
+  {
+    image: "italian.jpg",
+    name: "Italian",
+    price: 25,
+    article: "P6",
+    available: true,
+    category: "big",
+    description:
+      "Italian herbs, spicy pepperoni, tomato sauce, mushrooms, mozzarella, black olives",
+  },
+  {
+    image: "mexican.jpg",
+    name: "Mexican",
+    price: 30,
+    article: "P7",
+    available: true,
+    category: "big",
+    description:
+      "Chicken, tomato sauce, bell pepper, red onion, mozzarella, spicy jalapenos, tomatoes, salsa sauce",
+  },
+  {
+    image: "pepperoni.jpeg",
+    name: "Pepperoni",
+    price: 27,
+    article: "P8",
+    available: true,
+    category: "big",
+    description: "Spicy pepperoni, larger portion of mozzarella, tomato sauce",
+  },
+  {
+    image: "cheese-chicken.jpg",
+    name: "Cheese chicken",
+    price: 24,
+    article: "P9",
+    available: true,
+    category: "medium",
+    description: "Chicken, mozzarella, tomatoes, cheese sauce",
+  },
+  {
+    image: "four-seasons.jpg",
+    name: "Four seasons",
+    price: 18,
+    article: "P11",
+    available: true,
+    category: "big",
+    description:
+      "Larger portion of mozzarella, ham, pepperoni, feta cheese, tomatoes, champignons, tomato sauce",
+  },
+  {
+    image: "meat.jpg",
+    name: "Meat",
+    price: 22,
+    article: "P12",
+    available: true,
+    category: "big",
+    description:
+      "Chicken, ham, pepperoni, tomato sauce, spicy chorizo, mozzarella",
+  },
+  {
+    image: "four-cheeses.jpg",
+    name: "Four cheeses",
+    price: 13,
+    article: "P13",
+    available: true,
+    category: "small",
+    description:
+      "Larger portion of mozzarella, cheddar cheese, parmesan, blue cheese, cream sauce",
+  },
+  {
+    image: "double-pepperoni.jpg",
+    name: "Double pepperoni",
+    price: 16,
+    article: "P14",
+    available: true,
+    category: "medium",
+    description:
+      "More spicy pepperoni, larger portion of mozzarella, tomato sauce",
+  },
+  {
+    image: "veggie.jpg",
+    name: "Veggie",
+    price: 21,
+    article: "P15",
+    available: false,
+    category: "medium",
+    description:
+      "Tomatoes, feta cheese, champignons, bell peppers, red onions, mozzarella, black olives, tomato sauce",
+  },
+  {
+    image: "margherita.jpg",
+    name: "Margherita",
+    price: 11,
+    article: "P16",
+    available: true,
+    category: "big",
+    description: "Larger portion of mozzarella, tomatoes, tomato sauce",
+  },
+  {
+    image: "crazy-pepperoni.jpg",
+    name: "Crazy pepperoni",
+    price: 19,
+    article: "P17",
+    available: true,
+    category: "small",
+    description:
+      "Chicken, spicy pepperoni, tomato sauce, mozzarella, sweet and sour sauce",
+  },
+  {
+    image: "chicken-barbecue.jpg",
+    name: "Chicken barbecue",
+    price: 25,
+    article: "P18",
+    available: true,
+    category: "medium",
+    description:
+      "Chicken, barbecue sauce, tomato sauce, red onion, mozarella, bacon",
+  },
+]; // adding Helmet to enhance your API's security
 
-app.use(helmet());
+// app.use(helmet());
 app.use(serveStatic(path.join(__dirname, "dist"))); // using bodyParser to parse JSON bodies into JS objects
 
 app.use(bodyParser.json()); // enabling CORS for all requests
@@ -173,7 +200,7 @@ app.get("/products", function (req, res) {
 app.post("/form", function (req, res) {
   if (req) {
     res.send({
-      message: "got form"
+      message: "got form",
     });
   }
 }); // starting the server
--- src/vuex/actions/api-requests.js
+++ src/vuex/actions/api-requests.js
@@ -2,29 +2,25 @@ import axios from "axios";
 
 export default {
   GET_PRODUCTS_FROM_API({ commit }) {
-    return (
-      axios("http://localhost:3001/products", {
-        method: "GET",
+    return axios("http://localhost:8080/products", {
+      method: "GET",
+      withCredentials: false,
+    })
+      .then((data) => {
+        if (data) {
+          console.log(data.data);
+          data.data.forEach(function (item) {
+            item.quantity = 1;
+          });
+          commit("SET_PRODUCTS_TO_STATE", data.data);
+          return data.data;
+        }
       })
-        //   .then((data) => {
-        //     JSON.parse(data);
-        //   })
-        .then((data) => {
-          if (data) {
-            console.log(data.data);
-            data.data.forEach(function (item) {
-              item.quantity = 1;
-            });
-            commit("SET_PRODUCTS_TO_STATE", data.data);
-            return data.data;
-          }
-        })
-        .catch((error) => {
-          commit("SET_OFFLINE_DATA");
-          console.log(error);
-          return error;
-        })
-    );
+      .catch((error) => {
+        commit("SET_OFFLINE_DATA");
+        console.log(error);
+        return error;
+      });
   },
   GET_CURRENCY_RATES({ commit }) {
     return axios("https://api.exchangeratesapi.io/latest", {
@@ -46,9 +42,9 @@ export default {
 
     axios({
       method: "post",
-      url: "http://localhost:3001/form",
+      url: "http://localhost:8080/form",
       data: form,
-      validateStatus: () => true,
+      withCredentials: false,
     })
       .then((res) => {
         if (res) {
@@ -62,3 +58,6 @@ export default {
       });
   },
 };
+
+
+
